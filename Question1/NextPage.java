121726c4f8d3575084d26c2a08584787bc3532e4
Gajula Trilokesh Sai Veera Manikanta <68163381+lokeshgajula@users.noreply.github.com>
2021-06-06 12:33:43 +0530

    Create NextPage.java

---
+++ Question1/NextPage.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+class NextPage extends JFrame
+{
+    NextPage()
+    {
+	setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+	setTitle("Welcome");
+	setSize(2000,1300);
+    }
+}
